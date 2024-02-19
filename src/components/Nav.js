d558291e35fc2a792599206e386976ddb533827f
Moon <syoung90@naver.com>
2024-02-19 20:03:45 +0900

    '5536'

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from'styled-components'
 
 const Nav = () => {
+
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if(window.scrollY > 50){
+        setShow(true);
+      } else{
+        setShow(false);
+      }
+    })
+    return () => {
+      window.removeEventListener('scroll', () => {});
+    }
+  }, [])
+
+
+
+
+
   return (
-    <NavWrapper>
+    <NavWrapper show ={show}>
       <Logo>
-        <img alt='Disney Plus Logo' src='/images/logo.svg'onClick={() => (window.location.href = "/")} />
+        <img alt='Disney Plus Logo'
+             src='/images/logo.svg'
+             onClick={() => (window.location.href = "/")}
+        />
       </Logo>
     </NavWrapper>
   )
@@ -20,7 +43,7 @@ const NavWrapper = styled.nav`
   left: 0;
   right: 0;
   height: 70px;
-  background-color: #090b13;
+  background-color: ${props => props.show ? "#090b13" : "transparent"};
   display: flex;
   justify-content: space-between
   align-items: center;
