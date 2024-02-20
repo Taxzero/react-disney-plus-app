134553a73f98e36a0a3f9cda314b7ebed87a3788
Moon <syoung90@naver.com>
2024-02-20 20:19:46 +0900

    '011003'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
+import styled from 'styled-components';
 import './App.css';
 import  Nav  from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
+    <Container>
       <Nav />
-    </div>
+    </Container>
   );
 }
 
 export default App;
+
+
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw +5px);
+
+  &:after{
+    background: url("/images/home-background.png") center center no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+  `
