dd6860e2e94e17c9c8aacb7eecfb9b82a5a079cf
Moon <syoung90@naver.com>
2024-03-17 17:14:28 +0900

    '035106'

--- src/components/MovieModel/index.js
+++ src/components/MovieModel/index.js
@@ -12,7 +12,28 @@ const MovieModal = ({
     setModalOpen
 }) => {
   return (
-    <div>MovieModal</div>
+    <div className='presentation' role='presentation'>
+      <div className='wrapper-model'>
+        <span
+          onClick={()=> setModalOpen(false)}
+          className='modal-close'>
+            x
+        </span>
+        <img
+          className='modal__poster-img'
+          src={`https:/image.tmdb.org/t/p/original/${backdrop_path}`}
+          alt='modal-img' />
+          <div className='modal__content'>
+            <p className='modal__details'>
+              <span className='modal__user_perc'>100% for you</span>{" "}
+              {release_date ? release_date : first_air_date}
+            </p>
+            <h2 className='modal__title'>{title ? title : name}</h2>
+            <p className='modal__overview'>평점: {vote_average}</p>
+            <p className='modal__overview'>{overview}</p>
+          </div>
+      </div>
+    </div>
   )
 }
 
